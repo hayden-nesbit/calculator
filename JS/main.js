76504a9ae2842468d919d66e056ef662fe82fa59
Hayden Nesbit <hayden.nesbit@campusoutreach.org>
2020-03-13 09:52:03 -0400

    scope issue resolved

--- JS/main.js
+++ JS/main.js
@@ -11,20 +11,57 @@ class Model {
     constructor() {
         this.view = null;
         this.store = "";
+        this.display = 0
+        this.array = [];
     };
 
     setView(view) {
         this.view = view;
     }
 
-    updateView(str) {
-        this.view.updateView(str);
-    }
+    // updateView(str) {
+    //     this.view.updateView(str);
+    // }
 
     clear() {
         this.store = "0"
         this.view.updateDisplay(this.store);
     }
+
+    calculate(e) {
+        // this is the Model
+        let numbers = ("0123456789");
+        let operators = ("+x%-=");
+
+        if (e.target.textContent === "C") {
+            this.clear()
+            this.view.updateDisplay(this.store);
+        } else {
+            this.store += e.target.textContent;
+            this.view.updateDisplay(this.store)
+        }
+        for (let i = 0; i < this.store.length; i++) {
+            for (let j = 0; j < operators.length; j++) {
+                if (this.store[i] === operators[i]) {
+                    this.store.split(operators[i])
+                        // this.array
+                }
+                console.log(this.store)
+            }
+
+        }
+
+        // } else if (!(isNaN(e.target.textContent))) {
+        //     this.controller.model.store += e.target.textContent;
+        //     this.updateDisplay(Number(this.controller.model.store))
+        // } else
+        // if (e.target.charCode === 37 || e.target.charCode === 43 || e.target.charCode === 45 || e.target.charCode === 120 || e.target.charCode === 61) {
+        //     this.controller.model.store += e.target.charCode;
+        //     this.updateDisplay(this.controller.model.store);
+        // }
+
+    }
+
 }
 
 // <------------------------------------------CONTROLLER OBJECT (BUTTONS)--------------------------------------------->
@@ -33,11 +70,11 @@ class Controller {
     constructor(model) {
         this.model = model
         this.currentValue = 0;
-        this.__this__ = this
     };
 
-    numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
-    operators = ("+", "x", "%", "-", "=")
+    handleClick(e) {
+        this.model.calculate(e);
+    }
 
 
 }
@@ -47,7 +84,6 @@ class Controller {
 class View {
     constructor(controller) {
         this.controller = controller;
-        this.display = 0
     };
 
     buildElement(elementType, classes, id, textContent) {
@@ -59,18 +95,8 @@ class View {
     }
 
     updateDisplay(currentValue) {
-        this.display = currentValue
-        document.getElementById("output").innerHTML = this.display
-    }
-
-    numberClick(e) {
-        if (e.target.textContent === "C") {
-            this.controller.model.clear()
-            this.updateDisplay(this.controller.model.store);
-        } else {
-            this.controller.model.store += e.target.textContent;
-            this.updateDisplay(Number(this.controller.model.store))
-        }
+        this.controller.model.display = currentValue
+        document.getElementById("output").innerHTML = this.controller.model.display
     }
 
     buildCalculator() {
@@ -93,7 +119,7 @@ class View {
         let rowA = this.buildElement("div", "row", "", "")
         mainDiv.appendChild(rowA)
 
-        let colB = this.buildElement("div", "col-md-12 rounded mb-2 text-right", "output", this.display)
+        let colB = this.buildElement("div", "col-md-12 rounded mb-2 text-right", "output", this.controller.model.display)
         rowA.appendChild(colB)
 
         let rowB = this.buildElement("div", "row", "", "")
@@ -108,7 +134,7 @@ class View {
             for (let j = 0; j < 4; j++) {
                 col = this.buildElement("button", "col rounded-circle h3 m-1 text-white bg-dark", "btn" + k, calcBtns[k]);
                 if (!(k === 1 || k === 2 || k === 17)) {
-                    col.addEventListener("click", this.numberClick.bind(this));
+                    col.addEventListener("click", this.controller.handleClick.bind(this.controller));
                 }
                 if (k === 3 || k === 7 || k === 11 || k === 15 || k === 19) {
                     col.classList.remove("bg-dark")
